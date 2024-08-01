b36b8c0943e48e552ffb39841f416cd60cb6ce51
JanEricLink <145751242+JanEricLink@users.noreply.github.com>
2024-08-01 13:42:35 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,32 +12,39 @@ let amp;
 let freq;
 let ampSlider;
 let freqSlider;
+let graphSelecter;
 let sortedKeys;
+let maxValue;
 
 function setup() {
-  frameRate(60);
   angleMode(DEGREES);
 
   inputs();
-
-  a = aStart;
-  points = {};
+  reset();
+  //noLoop();
 }
 
 function draw() {
-  getInputs();
+  //setTimeout(redraw, 0);
+
+  getUserInputs();
 
   p1 = createVector(amp * 50 * sin(a), +amp * 50 * cos(a)).add(p0); //Point on Circle
   p2 = createVector(amp * 50 * sin(a), 0).add(p0); //Point on x-Achsis
   p3 = createVector(0, amp * 50 * cos(a)).add(p0); //Point on y-Achsis
-  p4 = createVector(amp * 50, amp * 50 * cos(a)).add(p0); //Point on Verticalizer
-  p5 = createVector((a - aStart) / freq + amp * 50, amp * 50 * cos(a)).add(p0); //Point on Sin
+  graphCalculation();
 
   points[p5.x] = p5.y;
 
+  graphSelecter.input(changeToSelected);
+
   malmal();
 
-  debug();
+  graphSelecter.position(10, 10);
+  drawSelecter.position(10, 40);
+  if (debugCheckbox.checked()) {
+    debug();
+  }
 
   a += freq;
   if (a >= len * 360 + aStart) {
@@ -74,14 +81,31 @@ function malmal() {
   text("Frequenz: " + freq, width - 200, 120);
 
   let previousKey = null;
-  for (const key in points) {
-    strokeWeight(2);
-    point(key, points[key]);
-    strokeWeight(2);
-    if (previousKey !== null) {
-      line(previousKey, points[previousKey], key, points[key]);
+  for (var key in points) {
+    switch (drawSelecter.selected()) {
+      case "Dotted":
+        strokeWeight(2);
+        point(key, points[key]);
+        break;
+      case "Connected":
+        strokeWeight(2);
+        if (previousKey !== null) {
+          if (
+            graphSelecter.selected() != "Tan" ||
+            points[previousKey] > points[key]
+          ) {
+            line(previousKey, points[previousKey], key, points[key]);
+          }
+        }
+        previousKey = key;
+        break;
+      case "Filled":
+        strokeWeight(2);
+        line(key, points[key], key, windowHeight / 2);
+        break;
+      default:
+        break;
     }
-    previousKey = key;
   }
 
   stroke(color(255, 0, 0)); //Rot
@@ -96,19 +120,33 @@ function malmal() {
 }
 
 function inputs() {
-  createCanvas(800, 400);
+  createCanvas(windowWidth, windowHeight - 1 / 3);
+  p0 = createVector(150, height / 2);
   aStart = 90;
-  len = 5;
   amp = 1;
   freq = 1;
+  len = ((windowWidth - amp * 50 - p0.x) * freq) / 360;
   ampSlider = createSlider(0.1, 2.5, 1, 0.1);
   ampSlider.position(width - 200, 30);
-  freqSlider = createSlider(0.1, 10, 1, 0.3);
+  freqSlider = createSlider(0.5, 20, 1, 0.5);
   freqSlider.position(width - 200, 80);
-  p0 = createVector(150, height / 2);
+  graphSelecter = createSelect();
+  graphSelecter.position(10, 10);
+  graphSelecter.option("Sin");
+  graphSelecter.option("Cos");
+  graphSelecter.option("Tan");
+  graphSelecter.selected("Sin");
+  drawSelecter = createSelect();
+  drawSelecter.position(10, 40);
+  drawSelecter.option("Dotted");
+  drawSelecter.option("Connected");
+  drawSelecter.option("Filled");
+  drawSelecter.selected("Dotted");
+  debugCheckbox = createCheckbox("Debug Mode");
+  debugCheckbox.position(windowWidth - 120, windowHeight - 30);
 }
 
-function getInputs() {
+function getUserInputs() {
   if (amp != ampSlider.value()) {
     reset();
     amp = ampSlider.value();
@@ -116,21 +154,97 @@ function getInputs() {
   if (freq != freqSlider.value()) {
     reset();
     freq = freqSlider.value();
+    len = ((windowWidth - amp * 50 - p0.x) * freq) / 360;
   }
 }
 
 function reset() {
   background(240);
-  points = {};
   a = aStart;
+  points = {};
 }
 
 function debug() {
+  graphSelecter.position(10, 100);
+  drawSelecter.position(10, 130);
   strokeWeight(1);
   stroke(255, 0, 0);
-  text(a, 0, 10);
-  text(p5.x, 0, 20);
-  text(frameRate(), 0, 40);
-  text(mouseX, 10, height - 10);
-  text(mouseY, 40, height - 10);
+  text("Angle: " + a, 10, 20);
+  text("p5.x: " + p5.x, 10, 40);
+  text("FPS: " + round(frameRate(), 0), 10, 60);
+  text("Maus Y Pos: " + round(mouseY), 10, windowHeight - 10);
+  text("Maus X Pos: " + round(mouseX), 10, windowWidth - 30);
+  text("len: " + len, 10, 80);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  ampSlider.position(windowWidth - 200, 30);
+  freqSlider.position(windowWidth - 200, 80);
+  len = ((windowWidth - amp * 50 - p0.x) * freq) / 360;
+  debugCheckbox.position(windowWidth - 120, windowHeight - 30);
+}
+
+function changeToSelected() {
+  switch (graphSelecter.selected()) {
+    case "Sin":
+      //Custom Values for each graph here
+      aStart = 90;
+      reset();
+      break;
+
+    case "Cos":
+      //Custom Values for each graph here
+      aStart = 180;
+      reset();
+      break;
+
+    case "Tan":
+      //Custom Values for each graph here
+      aStart = 90;
+      reset();
+      break;
+
+    default:
+      print(
+        "Es ist ein unbekannter fehler beim ändern des Graphen aufgetreten. Bitte Seite neu laden."
+      );
+      break;
+  }
+}
+
+function graphCalculation() {
+  switch (graphSelecter.selected()) {
+    case "Sin":
+      p4 = createVector(amp * 50, amp * 50 * cos(a)).add(p0); //Point on Verticalizer
+      p5 = createVector((a - aStart) / freq + amp * 50, amp * 50 * cos(a)).add(
+        p0
+      ); //Point on Sin
+      break;
+
+    case "Cos":
+      p4 = createVector(amp * 50, amp * 50 * cos(a)).add(p0); //Point on Verticalizer
+      p5 = createVector((a - aStart) / freq + amp * 50, amp * 50 * cos(a)).add(
+        p0
+      ); //Point on cos
+      break;
+
+    case "Tan":
+      let tanValue = tan(a - aStart);
+      if (abs(tanValue) > maxValue) {
+        tanValue = maxValue * Math.sign(tanValue);
+      }
+      p4 = createVector(amp * 50, amp * 50 * -tan(a - aStart)).add(p0); //Point on Verticalizer
+      p5 = createVector(
+        (a - aStart) / freq + amp * 50,
+        amp * 50 * -tan(a - aStart)
+      ).add(p0); //Point on Tan
+      break;
+
+    default:
+      print(
+        "Es ist ein unbekannter fehler beim ändern des Graphen aufgetreten. Bitte Seite neu laden."
+      );
+      break;
+  }
 }
